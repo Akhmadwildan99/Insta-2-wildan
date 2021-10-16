7374463dfdf384f2ae879c4d52ea5afa50ead11a
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-10-16 17:51:58 +0700

    build the stories

--- components/Stories.js
+++ components/Stories.js
@@ -1,15 +1,28 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import faker from 'faker';
+import Story from './Story'
 
 function Stories() {
+    const [sugestions, setSugestions] = useState([]);
+
     useEffect(()=>{
         const sugestions = [...Array(20)].map((_, i) => ({
             ...faker.helpers.contextualCard(), id: i
         }));
-        console.log(sugestions);
+        setSugestions(sugestions);
     },[])
     return (
-        <div>
+        <div className="flex space-x-2 p-6 bg-white border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black
+         ">
+            {sugestions.map(profile => {
+                return (
+                    <Story 
+                    key={profile.id}
+                    username={profile.username}
+                    img={profile.avatar}
+                     />
+                )
+            })}
             {/* Story */}
             {/* Story */}
             {/* Story */}
