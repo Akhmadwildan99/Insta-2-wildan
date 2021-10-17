0f2d60d7fec5525e1cf6d215e5a0bb9810471fc5
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-10-17 21:03:01 +0700

    authentikasi and authorisasi

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import {useEffect, useState} from 'react';
 import faker from 'faker';
-import Story from './Story'
+import Story from './Story';
+import {useSession} from 'next-auth/react'
 
 function Stories() {
     const [sugestions, setSugestions] = useState([]);
+    const {data: session} = useSession();
 
     useEffect(()=>{
         const sugestions = [...Array(20)].map((_, i) => ({
@@ -14,6 +16,8 @@ function Stories() {
     return (
         <div className="flex space-x-2 p-6 bg-white border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black
          ">
+            {session &&
+            <Story img={session.user.image} username={session.user.username} />}
             {sugestions.map(profile => {
                 return (
                     <Story 
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,17 +1,27 @@
-import {getProviders, signIn as SignintoProviders} from 'next-auth/react'
+import {getProviders, signIn as SignintoProviders} from 'next-auth/react';
+import Header from '../../components/Header'
 
 function signIn({providers}) {
     return (
        <>
-       {
-           Object.values(providers).map((provider) => (
-                   <div key={provider.name}>
-                       <button onClick={() => SignintoProviders(provider.id)}>
-                           Sign in with {provider.name}
-                       </button>
-                  </div>  
-           ))
-       }
+       <Header />
+       <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-16 px-14 text-center">
+        <img className="w-80" src="https://links.papareact.com/ocw"/>
+        <p className="font-sm italic">This Instagram I build to my portofolio career</p>
+        <div className="mt-40">
+            {
+                Object.values(providers).map((provider) => (
+                        <div className="" key={provider.name}>
+                            <button className=" p-3 bg-blue-500 text-white  rounded-lg " onClick={() => SignintoProviders(provider.id, {callbackUrl: '/'})}>
+                                SignIn with {provider.name}
+                            </button>
+                        </div>  
+                ))
+            }
+
+        </div>
+
+       </div>
        </>
     )
 }
