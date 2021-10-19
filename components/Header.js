11389c0ac84840bb7be0e5a62bc99cdc6550e57c
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-10-19 08:57:28 +0700

    read data post from firestor to feed insta

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,6 @@ function Header() {
     const {data: session} = useSession();
     const [open, setOpen] = useRecoilState(modalState);
     const router = useRouter();
-    console.log(session)
     return (
         <div className="shadow-sm border-b bg-white sticky top-0 z-50">
             <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
--- components/Posts.js
+++ components/Posts.js
@@ -1,32 +1,43 @@
 import Post from "./Post";
+import {useEffect, useState} from 'react';
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
+import { db } from "../firebase";
 
-const posts = [
-    {
-        id: '123',
-        username: 'Akhmad',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        captions: 'THIS IS DOPE'
-    },
-    {
-        id: '124',
-        username: 'Akhmad',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        captions: 'THIS IS DOPE'
-    },
-]
+// const posts = [
+//     {
+//         id: '123',
+//         username: 'Akhmad',
+//         userImg: 'https://links.papareact.com/3ke',
+//         img: 'https://links.papareact.com/3ke',
+//         captions: 'THIS IS DOPE'
+//     },
+//     {
+//         id: '124',
+//         username: 'Akhmad',
+//         userImg: 'https://links.papareact.com/3ke',
+//         img: 'https://links.papareact.com/3ke',
+//         captions: 'THIS IS DOPE'
+//     },
+// ]
 function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        onSnapshot(query(collection(db, "post"), orderBy("timeStamp", "desc")), snapshot => {
+            setPosts(snapshot.docs);
+        });
+    }, [db]);
+ 
     return (
         <div>
             {posts.map((post) => {
                 return (
                     <Post 
                     key={post.id} 
-                    username={post.username}
-                    userImg={post.userImg}
-                    captions={post.captions}
-                    img={post.img}/>
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    captions={post.data().caption}
+                    img={post.data().image}/>
                     
                 )
             })}
